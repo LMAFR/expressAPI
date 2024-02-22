de8b039bc9ab02ad3d7ea140e325cd95bcecd11d
LMAFR <alejandrofloridoreyes@gmail.com>
2024-02-22 10:37:23 +0100

    Migrated HTTP calls to a router folder.

--- miduapp.js
+++ miduapp.js
@@ -1,17 +1,6 @@
 import express, { json } from 'express' // require -> commonJS
-import { randomUUID } from 'node:crypto'
 import cors from 'cors'
-
-// import movies from './movies.json' // Esto no funciona en ESModules
-import { validateMovie, validatePartialMovie } from './movies.js'
-
-// Cómo leer JSON en ESModules
-// 1) Usando FS
-// import fs from 'node:fs'
-// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
-// 2) Recomendado. Creando la función require (ver utils.js)
-import { readJSON } from './utils.js'
-const movies = readJSON('./movies.json')
+import { moviesRouter } from './router/movies.js'
 
 const app = express()
 app.use(json())
@@ -43,82 +32,8 @@ app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
 // CORS PRE-Flight
 // OPTIONS
 
-// Todos los recursos que sean MOVIES se identifica con /movies
-app.get('/movies', (req, res) => {
-  const { genre } = req.query
-  if (genre) {
-    const filteredMovies = movies.filter(
-      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-    )
-    return res.json(filteredMovies)
-  }
-  res.json(movies)
-})
-
-app.get('/movies/:id', (req, res) => {
-  const { id } = req.params
-  const movie = movies.find(movie => movie.id === id)
-  if (movie) return res.json(movie)
-  res.status(404).json({ message: 'Movie not found' })
-})
-
-app.post('/movies', (req, res) => {
-  const result = validateMovie(req.body)
-
-  if (!result.success) {
-    // 422 Unprocessable Entity
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  // en base de datos
-  const newMovie = {
-    id: randomUUID(), // uuid v4
-    ...result.data
-  }
-
-  // Esto no sería REST, porque estamos guardando
-  // el estado de la aplicación en memoria
-  movies.push(newMovie)
-
-  res.status(201).json(newMovie)
-})
-
-app.delete('/movies/:id', (req, res) => {
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  movies.splice(movieIndex, 1)
-
-  return res.json({ message: 'Movie deleted' })
-})
-
-app.patch('/movies/:id', (req, res) => {
-  const result = validatePartialMovie(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data
-  }
-
-  movies[movieIndex] = updateMovie
-
-  return res.json(updateMovie)
-})
+// Todos los recursos que sean MOVIES se identifica con /movies. Actualmente nos traemos todo del archivo /router/movies.js
+app.use('/movies', moviesRouter)
 
 // Esta línea es importante en despliegues porque no sabemos el puerto al que se quiere conectar el hosting que realiza el despliegue, pero normalmente usará process.env para
 // realizar la conexión. Si dejamos PORT = 1234, por ejemplo, obtendremos un error al intentar desplegar, ya que normalmente el hosting no usará ese puerto.
---
+++ router/movies.js
@@ -0,0 +1,92 @@
+import { Router } from 'express'
+// import movies from './movies.json' // Esto no funciona en ESModules
+import { readJSON } from './utils.js'
+import { validateMovie, validatePartialMovie } from '../movies.js'
+import { randomUUID } from 'node:crypto'
+
+const router = Router()
+
+// Cómo leer JSON en ESModules
+// 1) Usando FS
+// import fs from 'node:fs'
+// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
+// 2) Recomendado. Creando la función require (ver utils.js)
+const movies = readJSON('./movies.json')
+
+// En este caso se toma como referencia el puerto + /movies (esta lógica la metemos en el miduapp.js), por lo que solo pondremos
+// lo que iría tras el /movies en las URLs de las llamadas que pongamos aquí
+router.get('/', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+router.post('/', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (!result.success) {
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  // en base de datos
+  const newMovie = {
+    id: randomUUID(), // uuid v4
+    ...result.data
+  }
+
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+router.patch('/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
