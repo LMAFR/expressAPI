59b567556a85fba8194e4a0094d68cf0f0749435
LMAFR <alejandrofloridoreyes@gmail.com>
2024-02-22 09:30:39 +0100

    Added files from lesson 3

---
+++ movies.js
@@ -0,0 +1,37 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required'
+  }),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(5), // default nos pone un valor por defecto si no le pasamos este parámetro a la request
+  poster: z.string().url(
+    { message: 'Poster must be a valid URL' }
+  ),
+  genre: z.array(
+    z.enum(['Action', 'Crime', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+    {
+      required_error: 'Movie genre is required.',
+      invalid_type_error: 'Movie genre must be an array of enum Genre'
+    }
+  )
+})
+
+function validateMovie (input) {
+  return movieSchema.safeParse(input)
+}
+
+function validatePartialMovie (input) {
+  // partial hace que todas las propiedades del esquema sean opcionales, de modo que solo se validarán las propiedades
+  // del esquema que hayamos pasado en el input, sin que devolver errores por el hecho de no estar todas.
+  return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
